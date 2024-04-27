55103f74ee548c7a550de078083096450c1617e4
duyledino <161328838+duyledino@users.noreply.github.com>
2024-04-27 20:51:27 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const myImage = ['dice1','dice2','dice3','dice4','dice5','dice6'];
 
 var setDice1 = document.querySelector('.img1');
 var setDice2 = document.querySelector('.img2');
+var setDice3 = document.querySelector('.img3');
 
 // if(randomNumber1>randomNumber2){
 //     getTitle.innerHTML = '	&#128681'+" Player 1 Wins ";
@@ -14,20 +15,23 @@ var setDice2 = document.querySelector('.img2');
 function generate(){
     var randomNumber1= Math.floor(Math.random()*6)+0; 
     var randomNumber2= Math.floor(Math.random()*6)+0; 
+    var randomNumber3= Math.floor(Math.random()*6)+0; 
 
     var random1 = randomNumber1 + 1;
     var random2 = randomNumber2 + 1;
+    var random3 = randomNumber3 + 1;
     
     var selectImg1 = myImage[randomNumber1];
     var selectImg2 = myImage[randomNumber2];
+    var selectImg3 = myImage[randomNumber3];
     setDice1.setAttribute('src','./'+selectImg1+'.png');
     setDice2.setAttribute('src','./'+selectImg2+'.png');
-
+    setDice3.setAttribute('src','./'+selectImg3+'.png');
 // setDice1.src = images[2];
 
     var getTitle = document.querySelector('h1');
 
-    var sum = random1+random2;
+    var sum = random1+random2+random3;
 
     if(sum%2==0){
         getTitle.innerHTML = sum + " nút ";
